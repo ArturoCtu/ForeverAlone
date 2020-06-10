c4ee265332e881913653353df0b5f44e2cc10ca4
ArturoTucan <a01196412@itesm.mx>
2020-06-09 23:45:42 -0500

    Arreglos
    Añadidas operaciones con arreglos.
    
    El arreglo siempre debe ir primero.

--- NoFunctions.c
+++ NoFunctions.c
@@ -7,28 +7,12 @@ var int j, k, l, it, arreglo[3];
 	float arr[10], not;
 	char iniciales[3];
 {
-	a = 10;
 	arr[1] = 10;
 	arr[2] = 20;
-	arr[3] = 30;
-	arreglo[0] = 1002;
-	if(b > a || b == 5) then {
-		d = a - b / 2;
-	} else {
-		d = a + b * 2;
-	}
-	
-	l = 0;
-	while(l<4){
-		l  = l + 1;
-		print(l);
-	}
 
-	for it = 1, to it<=3 {
-		print(arr[it]);
-		it = it + 1;
-	}
-	print("hola", arr[1 + 1]);
-	print("d es igual a ", d);
+	a = arr[1];
+	a = arr[2] + a * 2 + 3;
+
+	print(a);
 }
 end
--- virtualMachine.py
+++ virtualMachine.py
@@ -31,124 +31,136 @@ class memory(object):
 #If para saber que tipo de variable es y donde meterla
 	def indexVar(self, address, value):
 		#Global
-		if(address >= 1000 and address <= 2999):
-			self.gint[address] = value
-		if(address >= 3000 and address <= 4999):
-			self.gfloat[address] = value
-		if(address >= 5000 and address <= 6999):
-			self.gchar[address] = value
-		if(address >= 7000 and address <= 8999):
-			pass#self.gstring[address] = value
-		if(address >= 9000 and address <= 10999):
-			self.gbool[address] = value
+		if(address >= 1000 and address <= 10999):
+			if(address >= 1000 and address <= 2999):
+				self.gint[address] = value
+			if(address >= 3000 and address <= 4999):
+				self.gfloat[address] = value
+			if(address >= 5000 and address <= 6999):
+				self.gchar[address] = value
+			if(address >= 7000 and address <= 8999):
+				pass#self.gstring[address] = value
+			if(address >= 9000 and address <= 10999):
+				self.gbool[address] = value
 		#Temporales Globales
-		if(address >= 11000 and address <= 12999):
-			self.tint[address] = value
-		if(address >= 13000 and address <= 14999):
-			self.tfloat[address] = value
-		if(address >= 15000 and address <= 16999):
-			self.tchar[address] = value
-		if(address >= 17000 and address <= 18999):
-			pass#self.tstring[address] = value
-		if(address >= 19000 and address <= 20999):
-			self.tbool[address] = value
+		if(address >= 11000 and address <= 20999):
+			if(address >= 11000 and address <= 12999):
+				self.tint[address] = value
+			if(address >= 13000 and address <= 14999):
+				self.tfloat[address] = value
+			if(address >= 15000 and address <= 16999):
+				self.tchar[address] = value
+			if(address >= 17000 and address <= 18999):
+				pass#self.tstring[address] = value
+			if(address >= 19000 and address <= 20999):
+				self.tbool[address] = value
 		#Locales
-		if(address >= 21000 and address <= 22999):
-			self.lint[address] = value
-		if(address >= 23000 and address <= 24999):
-			self.lfloat[address] = value
-		if(address >= 25000 and address <= 26999):
-			self.lchar[address] = value
-		if(address >= 27000 and address <= 28999):
-			pass#self.lstring[address] = value
-		if(address >= 29000 and address <= 30999):
-			self.lbool[address] = value
+		if(address >= 21000 and address <= 30999):
+			if(address >= 21000 and address <= 22999):
+				self.lint[address] = value
+			if(address >= 23000 and address <= 24999):
+				self.lfloat[address] = value
+			if(address >= 25000 and address <= 26999):
+				self.lchar[address] = value
+			if(address >= 27000 and address <= 28999):
+				pass#self.lstring[address] = value
+			if(address >= 29000 and address <= 30999):
+				self.lbool[address] = value
 		#Temporales Locales
-		if(address >= 31000 and address <= 32999):
-			self.tint[address] = value
-		if(address >= 33000 and address <= 34999):
-			self.tfloat[address] = value
-		if(address >= 35000 and address <= 36999):
-			self.tchar[address] = value
-		if(address >= 37000 and address <= 38999):
-			pass#self.tstring[address] = value
-		if(address >= 39000 and address <= 40999):
-			self.tbool[address] = value
+		if(address >= 31000 and address <= 40999):
+			if(address >= 31000 and address <= 32999):
+				self.tint[address] = value
+			if(address >= 33000 and address <= 34999):
+				self.tfloat[address] = value
+			if(address >= 35000 and address <= 36999):
+				self.tchar[address] = value
+			if(address >= 37000 and address <= 38999):
+				pass#self.tstring[address] = value
+			if(address >= 39000 and address <= 40999):
+				self.tbool[address] = value
 		#Constantes
-		if(address >= 41000 and address <= 42999):
-			self.cint[address] = value
-		if(address >= 43000 and address <= 44999):
-			self.cfloat[address] = value
-		if(address >= 45000 and address <= 46999):
-			self.cchar[address] = value
-		if(address >= 47000 and address <= 48999):
-			self.cstring[address] = value
-		if(address >= 49000 and address <= 50999):
-			self.cbool[address] = value
+		if(address >= 41000 and address <= 50999):
+			if(address >= 41000 and address <= 42999):
+				self.cint[address] = value
+			if(address >= 43000 and address <= 44999):
+				self.cfloat[address] = value
+			if(address >= 45000 and address <= 46999):
+				self.cchar[address] = value
+			if(address >= 47000 and address <= 48999):
+				self.cstring[address] = value
+			if(address >= 49000 and address <= 50999):
+				self.cbool[address] = value
 		#Pointers
-		if(address >= 51000 and address <= 52999):
-			self.pointers[address] = value
+		if(address >= 51000 and address <= 60999):
+			if(address >= 51000 and address <= 52999):
+				self.pointers[address] = value
 
 	#Mismo if pero ahora para regresar el valor
 	def getValue(self, address):
 		#Global
-		if(address >= 1000 and address <= 2999):
-			return int(self.gint[address])
-		if(address >= 3000 and address <= 4999):
-			return float(self.gfloat[address])
-		if(address >= 5000 and address <= 6999):
-			return self.gchar[address]
-		if(address >= 7000 and address <= 8999):
-			pass#return self.gstring[address]
-		if(address >= 9000 and address <= 10999):
-			return self.gbool[address]
+		if(address >= 1000 and address <= 10999):
+			if(address >= 1000 and address <= 2999):
+				return int(self.gint[address])
+			if(address >= 3000 and address <= 4999):
+				return float(self.gfloat[address])
+			if(address >= 5000 and address <= 6999):
+				return self.gchar[address]
+			if(address >= 7000 and address <= 8999):
+				pass#return self.gstring[address]
+			if(address >= 9000 and address <= 10999):
+				return self.gbool[address]
 		#Temporales Globales
-		if(address >= 11000 and address <= 12999):
-			return int(self.tint[address])
-		if(address >= 13000 and address <= 14999):
-			return float(self.tfloat[address])
-		if(address >= 15000 and address <= 16999):
-			return self.tchar[address]
-		if(address >= 17000 and address <= 18999):
-			pass#return self.tstring[address]
-		if(address >= 19000 and address <= 20999):
-			return self.tbool[address]
+		if(address >= 11000 and address <= 20999):
+			if(address >= 11000 and address <= 12999):
+				return int(self.tint[address])
+			if(address >= 13000 and address <= 14999):
+				return float(self.tfloat[address])
+			if(address >= 15000 and address <= 16999):
+				return self.tchar[address]
+			if(address >= 17000 and address <= 18999):
+				pass#return self.tstring[address]
+			if(address >= 19000 and address <= 20999):
+				return self.tbool[address]
 		#Locales
-		if(address >= 21000 and address <= 22999):
-			return int(self.lint[address])
-		if(address >= 23000 and address <= 24999):
-			return float(self.lfloat[address])
-		if(address >= 25000 and address <= 26999):
-			return self.lchar[address]
-		if(address >= 27000 and address <= 28999):
-			pass#return self.lstring[address]
-		if(address >= 29000 and address <= 30999):
-			return self.lbool[address]
+		if(address >= 21000 and address <= 30999):
+			if(address >= 21000 and address <= 22999):
+				return int(self.lint[address])
+			if(address >= 23000 and address <= 24999):
+				return float(self.lfloat[address])
+			if(address >= 25000 and address <= 26999):
+				return self.lchar[address]
+			if(address >= 27000 and address <= 28999):
+				pass#return self.lstring[address]
+			if(address >= 29000 and address <= 30999):
+				return self.lbool[address]
 		#Temporales Locales
-		if(address >= 31000 and address <= 32999):
-			return int(self.tint[address])
-		if(address >= 33000 and address <= 34999):
-			return float(self.tfloat[address])
-		if(address >= 35000 and address <= 36999):
-			return self.tchar[address]
-		if(address >= 37000 and address <= 38999):
-			pass#return self.tstring[address]
-		if(address >= 39000 and address <= 40999):
-			return bool(self.tbool[address])
+		if(address >= 31000 and address <= 40999):
+			if(address >= 31000 and address <= 32999):
+				return int(self.tint[address])
+			if(address >= 33000 and address <= 34999):
+				return float(self.tfloat[address])
+			if(address >= 35000 and address <= 36999):
+				return self.tchar[address]
+			if(address >= 37000 and address <= 38999):
+				pass#return self.tstring[address] 
+			if(address >= 39000 and address <= 40999):
+				return bool(self.tbool[address])
 		#Constantes
-		if(address >= 41000 and address <= 42999):
-			return int(self.cint[address])
-		if(address >= 43000 and address <= 44999):
-			return float(self.cfloat[address])
-		if(address >= 45000 and address <= 46999):
-			return self.cchar[address]
-		if(address >= 47000 and address <= 48999):
-			return self.cstring[address]
-		if(address >= 49000 and address <= 50999):
-			return self.cbool[address]
+		if(address >= 41000 and address <= 50999):
+			if(address >= 41000 and address <= 42999):
+				return int(self.cint[address])
+			if(address >= 43000 and address <= 44999):
+				return float(self.cfloat[address])
+			if(address >= 45000 and address <= 46999):
+				return self.cchar[address]
+			if(address >= 47000 and address <= 48999):
+				return self.cstring[address]
+			if(address >= 49000 and address <= 50999):
+				return self.cbool[address]
 		#Pointers
-		if(address >= 51000 and address <= 52999):
-			return self.pointers[address]
+		if(address >= 51000 and address <= 60999):
+			if(address >= 51000 and address <= 52999):
+				return self.pointers[address]
 				
 memory = memory()
 #Reconstruye la memoria global que tenemos en un archivo
@@ -175,124 +187,133 @@ def era(funId):
 		data = json.load(infile)
 		nParams = data[funId]['nParams']
 
-	
+
 #Ejecuta el codigo de cuadruplos
 def excecute(quads):
 	rebuildCtes()
 	rebuildFunctionMemory('main')
-	print(*quads, sep = "\n")
+	#print(*quads, sep = "\n")
 	#Vamos al main
 	i = quads[0][3] 
 	#i -> quad pointer
 	while (i < len(quads)):
+		operator = quads[i][0]
+		pos1 = quads[i][1]
+		pos2 = quads[i][2]
+		pos3 = quads[i][3]
+
+		if pos3 != None:
+			if pos3 >= 51000:
+				pos3 = memory.getValue(pos3)
+		if pos2 != None:
+			if pos2 >= 51000:
+				pos2 = memory.getValue(pos2)
+		if pos1 != None:
+			if pos1 >= 51000:
+				pos1 = memory.getValue(pos1)
+
+
 		#Operaciones basicas
-		if quads[i][0] == 1:
-			res = memory.getValue(quads[i][1]) + memory.getValue(quads[i][2])
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 2:
-			res = memory.getValue(quads[i][1]) - memory.getValue(quads[i][2])
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 3:
-			res = memory.getValue(quads[i][1]) * memory.getValue(quads[i][2])
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 4:
-			res = memory.getValue(quads[i][1]) / memory.getValue(quads[i][2])
-			memory.indexVar(quads[i][3], res)
+		if operator == 1:
+			res = memory.getValue(pos1) + memory.getValue(pos2)
+			memory.indexVar(pos3, res)
+		elif operator == 2:
+			res = memory.getValue(pos1) - memory.getValue(pos2)
+			memory.indexVar(pos3, res)
+		elif operator == 3:
+			res = memory.getValue(pos1) * memory.getValue(pos2)
+			memory.indexVar(pos3, res)
+		elif operator == 4:
+			res = memory.getValue(pos1) / memory.getValue(pos2)
+			memory.indexVar(pos3, res)
 		#Operaciones Logicas
-		if quads[i][0] == 5:
-			if (memory.getValue(quads[i][1]) and memory.getValue(quads[i][2])):
+		elif operator == 5:
+			if (memory.getValue(pos1) and memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 6:
-			if (memory.getValue(quads[i][1]) or memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 6:
+			if (memory.getValue(pos1) or memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 7:
-			if (memory.getValue(quads[i][1]) > memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 7:
+			if (memory.getValue(pos1) > memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 8:
-			if (memory.getValue(quads[i][1]) < memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 8:
+			if (memory.getValue(pos1) < memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 9:
-			if (memory.getValue(quads[i][1]) >= memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 9:
+			if (memory.getValue(pos1) >= memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 10:
-			if (memory.getValue(quads[i][1]) <= memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 10:
+			if (memory.getValue(pos1) <= memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 11:
-			if (memory.getValue(quads[i][1]) != memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 11:
+			if (memory.getValue(pos1) != memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
-		if quads[i][0] == 12:
-			if (memory.getValue(quads[i][1]) == memory.getValue(quads[i][2])):
+			memory.indexVar(pos3, res)
+		elif operator == 12:
+			if (memory.getValue(pos1) == memory.getValue(pos2)):
 				res = True
 			else:
 				res = False
-			memory.indexVar(quads[i][3], res)
+			memory.indexVar(pos3, res)
 		#Asignacion
-		if quads[i][0] == 13:
-			if quads[i][1] < 51000:
-				res = memory.getValue(quads[i][3])
-				memory.indexVar(quads[i][1], res)
-			else:
-				#print(memory.getValue(quads[i][1]),'=',quads[i][3])
-				memory.indexVar(memory.getValue(quads[i][1]), memory.getValue(quads[i][3]))
+		elif operator == 13:
+			res = memory.getValue(pos3)
+			#print(pos1,'=',pos3)
+			memory.indexVar(pos1, res)
 		#Print
-		if quads[i][0] == 14:
-			if quads[i][3] < 51000:
-				res = memory.getValue(quads[i][3])
-				print(res)
-			else:
-				res = memory.getValue(memory.getValue(quads[i][3]))
-				print(res)
+		elif operator == 14:
+			#print(pos3)
+			res = memory.getValue(pos3)
+			print(res)
 		#Read
-		if quads[i][0] == 15:
+		elif operator == 15:
 			res = input()
-			memory.indexVar(quads[i][3], res)
+			memory.indexVar(pos3, res)
 		#Gotos
-		if quads[i][0] == 16:
-			i = quads[i][3]-1
-		if quads[i][0] == 17:
-			if(not memory.getValue(quads[i][1])):
-				i = quads[i][3]-1
-		if quads[i][0] == 18:
+		elif operator == 16:
+			i = pos3-1
+		elif operator == 17:
+			if(not memory.getValue(pos1)):
+				i = pos3-1
+		elif operator == 18:
 			pass
 		#Funciones
-		if quads[i][0] == 19:
-			era(quads[i][3])
-		if quads[i][0] == 20:
+		elif operator == 19:
+			era(pos3)
+		elif operator == 20:
 			pass
-		if quads[i][0] == 21:
+		elif operator == 21:
 			pass
-		if quads[i][0] == 22:
+		elif operator == 22:
 			pass
-		if quads[i][0] == 23:
+		elif operator == 23:
 			pass
 		#Ver
-		if quads[i][0] == 24:
+		elif operator == 24:
 			if memory.getValue(quads[i][1]) > quads[i][3]:
 				print("Arreglo Fuera de rango")
 				sys.exit()
-		if quads[i][0] == 25:
+		elif operator == 25:
 			res = quads[i][1] + memory.getValue(quads[i][2])
 			memory.indexVar(quads[i][3], res)
 		i+=1
-
